21f76ef3137e4b5880bf34ac570dc3e9d9b52284
nkwuda theophilus <theophilusonyebuchinkwuda@gmail.com>
2022-06-30 15:05:16 +0000

    commit

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,8 +1,8 @@
 #include <stdio.h>
 /**
- *main -  return the message from puts function
- *@msg: an operand 
-
+ *main - return the message from puts function
+ *@msg: a message 
+ *
  *return: 0 if succesful, 1 if oyherwisel
  */
 int main(void)

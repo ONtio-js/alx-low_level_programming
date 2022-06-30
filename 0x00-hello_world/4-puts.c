600590e6d784e3c1ea71f4eb9e2883f2fc3c7a1f
nkwuda theophilus <theophilusonyebuchinkwuda@gmail.com>
2022-06-30 14:46:40 +0000

    commit

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,10 @@
 #include <stdio.h>
-/*
+/**
  *function main return the message from puts function
  */
 int main(void)
 {
-	char msg[] = "\"Programming is like building a multilingua puzzle";
+	char msg[] = "\"Programming is like building a multilingual puzzle";
 
 	puts(msg);
 

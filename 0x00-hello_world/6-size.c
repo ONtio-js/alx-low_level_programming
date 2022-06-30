4d09e9681b1598806dd803ad4c1ae5a8d0012cdb
Nkwuda Theophilus Onyebuchi <103934885+ONtio-js@users.noreply.github.com>
2022-06-30 16:56:25 +0100

    Update 6-size.c

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -12,7 +12,7 @@ int main(void)
 	float s;
 
 	printf("Size of a char: %d byte(s)\n", sizeof(su));
-	printf("Size of an int: %d bytes(s)\n", sizeof(x));
+	printf("Size of an int: %d byte(s)\n", sizeof(x));
 	printf("Size of a long int: %d byte(s)\n", sizeof(y));
 	printf("Size of a long long int: %d byte(s)\n", sizeof(v));
 	printf("Size of a float: %d byte(s)\n", sizeof(s));

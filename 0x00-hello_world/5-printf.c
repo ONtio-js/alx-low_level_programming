aae5e274372df93a8e5c0561453ff8425879b9c7
nkwuda theophilus <theophilusonyebuchinkwuda@gmail.com>
2022-06-30 15:24:32 +0000

    printf function

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,9 @@
+/**
+ * main - prints message
+ * Return: 0 if successful,otherwise 1
+ */
+#include <stdio.h>
+int main(void)
+{	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}

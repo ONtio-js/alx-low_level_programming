2e35e2c598fe84bbebae5efc855714f3ed770c85
nkwuda theophilus <theophilusonyebuchinkwuda@gmail.com>
2022-06-30 16:21:49 +0000

    commit

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,11 @@
+/**
+ * main - returns the message
+ * Return: return 1
+ */
+#include <stdio.h>
+int main(void)
+{
+	fprintf("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
+
+	return (1);
+}

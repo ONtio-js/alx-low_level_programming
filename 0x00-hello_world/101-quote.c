324152acefb1e2efd21d62b00beb882be7cb7eea
nkwuda theophilus <theophilusonyebuchinkwuda@gmail.com>
2022-06-30 16:24:01 +0000

    commit

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -5,7 +5,7 @@
 #include <stdio.h>
 int main(void)
 {
-	fprintf("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
+	printf("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
 
 	return (1);
 }

e952bfcf061cb91e58864c6baa834548d07faa67
nkwuda theophilus <theophilusonyebuchinkwuda@gmail.com>
2022-07-01 13:51:32 +0000

    commit

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - return all the alphabet except e and q
+ * Return: return 0 if successful
+ */
+
+int main(void)
+{
+	int ch = 'a';
+
+	while (ch <= 'z')
+	{
+		if (ch == 'e' || ch == 'q')
+		{
+			continue;
+		}
+		else
+		{
+			putchar(ch);
+		}
+		ch++;
+		putchar('\n');
+		return (0);
+	}
+}

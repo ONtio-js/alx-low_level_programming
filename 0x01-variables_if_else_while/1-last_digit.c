1c589abaea5fe4d66eda3df36d4ccee3e9b1473b
nkwuda theophilus <theophilusonyebuchinkwuda@gmail.com>
2022-07-01 15:15:20 +0000

    commit

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -17,9 +17,9 @@ int main(void)
 	num = n % 10;
 	if (num > 5)
 	{
-		printf("%d and is greater and 5\n", num);
+		printf("%d and is greater than 5\n", num);
 	}
-	else if ((num < 6) && (num > 0))
+	else if ((num < 6) && (num < 0))
 	{
 		printf("%d and  is less than 6 and not 0 \n", num);
 	}

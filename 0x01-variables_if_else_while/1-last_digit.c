9c1b7cd64e20b16d1eb64cf97010fb235aebb27b
nkwuda theophilus <theophilusonyebuchinkwuda@gmail.com>
2022-07-01 15:11:22 +0000

    commit

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -14,7 +14,7 @@ int main(void)
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
 	printf("Last digit of %d is ", n);
-	num = ni % 10;
+	num = n % 10;
 	if (num > 5)
 	{
 		printf("%d and is greater and 5\n", num);

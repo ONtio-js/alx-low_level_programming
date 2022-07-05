ba12c5f243bd5c57c43464661b5a2b2187d8ef4c
nkwuda theophilus onyebuchi <theophilusonyebuchinkwuda@gmail.com>
2022-07-05 20:28:17 +0100

    commit

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * _islower - prints the alphabets in lower case
+ * Return: returns 0 if successful or 1 if otherwise
+ */
+
+int  _islower(int alpha)
+{
+	if(alpha <= 'a' && alpha >= 'z')
+	{
+		return (0);
+	}
+	else
+	{
+		return (1);
+	}
+}

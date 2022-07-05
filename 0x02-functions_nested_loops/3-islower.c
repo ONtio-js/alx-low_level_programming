96bece71ff6160ebaf825f35e85d79bf006ebd3b
nkwuda theophilus onyebuchi <theophilusonyebuchinkwuda@gmail.com>
2022-07-05 20:36:15 +0100

    commit

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -1,12 +1,13 @@
 #include "main.h"
 /**
- * _islower - prints the alphabets in lower case
+ * _islower - prints the alphabets in lower cas
+ * @alpha: number or an alphabet
  * Return: returns 0 if successful or 1 if otherwise
  */
 
 int  _islower(int alpha)
 {
-	if(alpha <= 'a' && alpha >= 'z')
+	if (alpha >= 'a' && alpha  <= 'z')
 	{
 		return (1);
 	}

739f91252a804d0854c4d9ca5a10ccc332eff353
nkwuda theophilus onyebuchi <theophilusonyebuchinkwuda@gmail.com>
2022-07-05 19:40:35 +0100

    alphabets

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * main - prints the alphabets in lower case
+ * Return: returns 0 if successful
+ */
+
+int main(void)
+{
+	char alphabet = 'a';
+
+	while (aphabet <= 'z')
+	{
+		-putchar(alphabet);
+		alphabet++;
+	}
+	_putchar('\n');
+	return (0);
+}

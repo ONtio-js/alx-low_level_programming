003a73958aa2983758283d04e5a4624c672609e5
nkwuda theophilus onyebuchi <theophilusonyebuchinkwuda@gmail.com>
2022-07-05 22:41:36 +0100

    commit

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * print_last_digit - prints the last digit of the given vakue
+ * @value: the given value
+ * Return: returns 0 if succcessful 1 if otherwise
+ */
+
+int print_last_digit(int value)
+{
+	if (value % 10 == 0)
+	{
+		putchar('0');
+	}
+	else
+	{
+		value = value % 10;
+		return (value)
+	}
+}

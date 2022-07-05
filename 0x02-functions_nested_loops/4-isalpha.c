cbc3797ead4a005898c518483b97e0cfe6b9625d
nkwuda theophilus onyebuchi <theophilusonyebuchinkwuda@gmail.com>
2022-07-05 20:47:08 +0100

    commit

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * _isalpha - check for an alphabetical letter
+ * @value: value for consideration
+ * Return: returns 1 if successful or 0 if otherwise
+ */
+
+int _islower (int value)
+{
+	if ((value >= 'a' || value >= 'A') && (value <= 'z' || value <= 'Z'))
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+}

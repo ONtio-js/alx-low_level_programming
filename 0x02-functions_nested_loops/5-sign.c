ee2967542fa0b157a93e366eda6b4d070142cb27
nkwuda theophilus onyebuchi <theophilusonyebuchinkwuda@gmail.com>
2022-07-05 20:58:58 +0100

    commit

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/**
+ * print_sign - return the supposed sign of an interger
+ * @value: the value to be evaluated
+ * Return: return 1 if positive 0 if value = 0 and -1 if negative
+ */
+
+int print_sign(int value)
+{
+	if (value > 0)
+	{
+		return (1);
+	}
+	else if (value = 0)
+	{
+		return (0);
+	}
+	else
+	{
+		return (-1);
+	}
+}

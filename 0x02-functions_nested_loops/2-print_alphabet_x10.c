067c98924281a8976df1b8700d4a14f2da6b37a4
nkwuda theophilus onyebuchi <theophilusonyebuchinkwuda@gmail.com>
2022-07-05 20:13:14 +0100

    commit

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -6,19 +6,18 @@
 
 void  print_alphabet_x10(void)
 {
-        char alphabet;
+	char alphabet;
 	int num = 1;
 
 	while (num <= 10)
 	{
 		alphabet = 'a';
-        	while (alphabet <= 'z')
-        	{
-        		 _putchar(alphabet);
-        		 alphabet++;
-        	}
+		while (alphabet <= 'z')
+		{
+			_putchar(alphabet);
+			alphabet++;
+		}
 		_putchar('\n');
 		num++;
-
 	}
 }

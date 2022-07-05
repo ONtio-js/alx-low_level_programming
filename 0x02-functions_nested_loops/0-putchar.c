2ae1a1313e38a0dd914e3b152e1e5577ac7a63ae
nkwuda theophilus onyebuchi <theophilusonyebuchinkwuda@gmail.com>
2022-07-05 19:10:13 +0100

    putchar function"

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * putchar - function that prints _putchar
+ * Return: returns 0 if successful
+ */
+void putchar(char);
+
+int main(void)
+{
+	putchar('_');
+	putchar('p');
+	putchar('u');
+	putchar('t');
+	putchar('c');
+	putchar('h');
+	putchar('a');
+	putchar('r');
+	putchar('\n');
+	return (0);
+}
---
+++ 0x02-functions_nested_loops/README.md
@@ -0,0 +1,3 @@
+NESTED LOOPS AND FUNCTIONS
+
+1.task one is a function that print _putchar

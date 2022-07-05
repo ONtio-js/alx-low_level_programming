64348878aa3f929ba0eeacc9f6b2edab4e785367
nkwuda theophilus onyebuchi <theophilusonyebuchinkwuda@gmail.com>
2022-07-05 21:23:20 +0100

    commit

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * _abs - returns the absolute value of a number given to it
+ * @value: a given value
+ * Return: return 0 if successful 1 if otherwise
+ */
+
+int _abs(int value)
+{
+	if (value < 0)
+	{
+		value = -(value);
+	}
+	else
+	{
+		value = value;
+	}
+	return (0);
+}

667956032540b4a3da4f89ec4d294e91ab536d68
nkwuda theophilus onyebuchi <theophilusonyebuchinkwuda@gmail.com>
2022-07-06 01:25:52 +0100

    commit

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -1,24 +1,48 @@
 #include "main.h"
 /**
- * times_table - prints the mutilplicative value of number from 0-9
+ * times_table -prints tables
+ *
+ * Return: Always 0.
  */
-
 void times_table(void)
 {
-	int a = 0;
-	int b;
-	
-	while (a <= 9)
-	{
-		b = 0;
-		while (b <= 9)
-		{
-			int c = a*b;
-			_putchar(c + '0');
-			_putchar(',');
-			b++;
-		}
-		_putchar('\n');
-		a++;
-	}
+int a;
+int b;
+int c;
+for (a = 0; a <= 9; a++)
+{
+for (b = 0; b <= 9; b++)
+{
+c = a * b;
+if ((c / 10) == 0)
+{
+if (b == 0)
+{
+_putchar ('0');
+}
+if (b != 0)
+{
+_putchar (' ');
+_putchar ((c % 10) + '0');
+}
+if (b < 9)
+{
+_putchar(',');
+_putchar (' ');
+}
+}
+else
+{
+_putchar ((c / 10) + '0');
+_putchar ((c % 10) + '0');
+if (b < 9)
+{
+_putchar(',');
+_putchar (' ');
+}
+}
+}
+_putchar ('\n');
+}
+}
 }

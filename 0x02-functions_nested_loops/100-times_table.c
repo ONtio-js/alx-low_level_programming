8346c446b3acaa706d333e645d2cf9a097d34881
nkwuda theophilus onyebuchi <theophilusonyebuchinkwuda@gmail.com>
2022-07-06 18:20:18 +0000

    commit

---
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -0,0 +1,5 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * print_times_table - prints the multiple of a number between 15 and 0;
+ * @value: the 
--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -1,48 +1,24 @@
 #include "main.h"
+#include <stdio.h>
 /**
  * times_table -prints tables
- *
- * Return: Always 0.
  */
 void times_table(void)
 {
-int a;
-int b;
-int c;
-for (a = 0; a <= 9; a++)
-{
-for (b = 0; b <= 9; b++)
-{
-c = a * b;
-if ((c / 10) == 0)
-{
-if (b == 0)
-{
-_putchar ('0');
-}
-if (b != 0)
-{
-_putchar (' ');
-_putchar ((c % 10) + '0');
-}
-if (b < 9)
-{
-_putchar(',');
-_putchar (' ');
-}
-}
-else
-{
-_putchar ((c / 10) + '0');
-_putchar ((c % 10) + '0');
-if (b < 9)
-{
-_putchar(',');
-_putchar (' ');
-}
-}
-}
-_putchar ('\n');
-}
-}
+	int a = 0;
+	int b;
+	int c;
+	
+	while (a <= 9)
+	{
+		b = 0;
+		while (b <= 0)
+		{
+			c = a * b;
+			printf("%d, ");
+			b++;
+		}
+		_putchar('\n');
+		a++;
+	}
 }

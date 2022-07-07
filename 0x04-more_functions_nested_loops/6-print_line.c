ed56e360287a59f10367334e19399a5c5c818472
nkwuda theophilus onyebuchi <theophilusonyebuchinkwuda@gmail.com>
2022-07-07 12:14:02 +0000

    cp

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,16 @@
+/**
+ * print_line - print a line in terminal
+ * @num: the length of the line
+ */
+#include "main.h"
+#include <stdio.h>
+
+void print_line(int num)
+{
+	int i;
+	for (i = 1; i <= num; i++)
+	{
+		putchar('_');
+	}
+	putchar('\n');
+}

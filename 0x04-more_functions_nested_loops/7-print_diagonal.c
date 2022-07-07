53515cc593cc045d5d130ca7b5f6bdb7367470e5
nkwuda theophilus onyebuchi <theophilusonyebuchinkwuda@gmail.com>
2022-07-07 12:48:30 +0000

    commit

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -10,10 +10,7 @@ void print_diagonal(int value)
 
 	for (i = 1; i <= value; i++)
 	{
-		for (j = 1; j <= i; j++)
-		{
-			_putchar('\v');
-		}
+		_putchar('\v');
 		_putchar('\\');
 	}
 	_putchar('\n');

ec6d1ac55889ef50af5d4ed711f4414e0e33e03b
nkwuda theophilus <theophilusonyebuchinkwuda@gmail.com>
2022-07-20 18:19:33 +0000

    recursion

--- 0x08-recursion/main.h
+++ 0x08-recursion/main.h
@@ -1,3 +1,4 @@
+int _putchar(char c);
 void _puts_recursion(char *s);
 void _print_rev_recursion(char *s);
 int _strlen_recursion(char *s);
@@ -5,6 +6,5 @@ int factorial(int n);
 int _pow_recursion(int x, int y);
 int _sqrt_recursion(int n);
 int is_prime_number(int n);
-void _putchar(char s);
 int wildcmp(char *s1, char *s2);
 int is_palindrome(char *s);

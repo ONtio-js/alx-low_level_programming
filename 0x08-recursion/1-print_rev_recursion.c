262c061ea9f08d693c4925507406da75f5aad305
nkwuda theophilus <theophilusonyebuchinkwuda@gmail.com>
2022-07-20 18:16:31 +0000

    recursion

---
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -0,0 +1,13 @@
+/**
+ * _print_rev_recursion - prints string in reverse other
+ * @s: the string to be print in reverse other
+ */
+
+void _print_rev_recursion(char *s)
+{
+	if (*s)
+	{
+		_print_rev_recursion(s + 1);
+		_putchar(*s);
+	}
+}

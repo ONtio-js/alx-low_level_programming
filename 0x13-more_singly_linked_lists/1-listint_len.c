0491942c9452fd2fc30421b377e8b73eb92904fb
nkwuda theophilus <theophilusonyebuchinkwuda@gmail.com>
2022-08-15 09:08:09 +0100

    more singly linked list

--- 0x13-more_singly_linked_lists/1-listint_len.c
+++ 0x13-more_singly_linked_lists/1-listint_len.c
@@ -1,26 +1,19 @@
-Bytes
-
 #include "lists.h"
 
 /**
- * listint_len - returns the number of elements in a linked lists
- * @h: linked list of type listint_t to traverse
- *
- * Return: number of nodes
+ * listint_len - number of elements list
+ * @h: pointer to structure
+ * Return: length.
  */
+
 size_t listint_len(const listint_t *h)
 {
-    size_t num = 0;
+unsigned int i = 0;
 
-    while (h)
-    {
-        num++;
-        h = h->next;
-    }
-
-    return (num);
+	while (h != NULL)
+	{
+		i++;
+		h = h->next;
+	}
+	return (i);
 }
-Footer
-© 2022 GitHub, Inc.
-Footer navigation
-

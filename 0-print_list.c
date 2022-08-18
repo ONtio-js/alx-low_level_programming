e9f40909b3bb05844ff25109b85ab7bae5844c0a
nkwuda theophilus <theophilusonyebuchinkwuda@gmail.com>
2022-08-18 13:31:32 +0100

    bit manipulation

---
+++ 0-print_list.c
@@ -0,0 +1,27 @@
+
+#include "lists.h"
+
+/**
+ * print_list - print elements of list
+ * @h: pointer to structure
+ * Return: length.
+ */
+
+
+
+size_t print_list(const list_t *h)
+{
+
+unsigned int i = 0;
+
+	while (h != NULL)
+	{
+		if (h->str != NULL)
+			printf("[%d] %s\n", h->len, h->str);
+		else
+			printf("[0] (nil)\n");
+		h = h->next;
+		i++;
+	}
+	return (i);
+}
